cd275811425a4382ffee0596a4f465bcfa20aecd
Valentine <valyuscha2004@gmail.com>
2020-06-10 16:01:14 +0300

    Make radio buttons

---
+++ src/components/sidebar/sidebar.template.js
@@ -0,0 +1,41 @@
+let buttonsWrapper
+
+export function createSidebar() {
+  const sidebar = document.createElement('div')
+  sidebar.classList.add('sidebar')
+  sidebar.innerHTML = createSidebarName()
+
+  buttonsWrapper = document.createElement('div')
+  buttonsWrapper.classList.add('sidebar__buttons')
+  buttonsWrapper.id = 'btnsWrapper'
+  createSidebarButtons()
+
+  sidebar.appendChild(buttonsWrapper)
+
+  return sidebar
+}
+
+function createSidebarName() {
+  return `
+    <h2 class="sidebar__name">
+      Countries of which region would you like to see?
+    </h2>
+  `
+}
+
+const sidebarButtons = ['All', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+
+function createSidebarButtons() {
+  return sidebarButtons.map(btn => {
+    const button = `
+      <div class="sidebar__button">
+        <button data-type="radio-btn" data-btn="btn"></button>
+        <p data-type="radio-btn" data-label="label">${btn}</p>
+      </div>
+    `
+
+    buttonsWrapper.insertAdjacentHTML('beforeend', button)
+
+    return buttonsWrapper
+  })
+}
